c3a66fb995bdf9e48f1606096ab1e45e5cf45e9f
KAYA jordan <kayajordan21@gmail.com>
2022-09-15 19:22:02 +0200

    adding path to add collaborator to menu

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 
 const Banner = () => {
 
@@ -18,6 +18,11 @@ const Banner = () => {
                         <Nav className={`me-auto`}>
                             <Nav.Link href="/collaborateurs">collaborateur<span className="text-white"></span></Nav.Link>
                         </Nav>
+                        {user.isAdmin &&
+                            <Nav className={`me-auto`}>
+                                <Nav.Link href="/ajoutercollaborateur">ajouter<span className="text-white"></span></Nav.Link>
+                            </Nav>
+                        }
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
---
+++ src/views/AddCollaborator/AddCollaborator.jsx
@@ -0,0 +1,19 @@
+import Banner from '../../components/Banner/Banner';
+import EditCreateForm from '../../components/EditCreateForm/EditCreateForm';
+
+
+const AddCollaborator = () => {
+
+  return (
+    <>
+      <Banner />
+      <div>
+        <h1>Ajouter collaborateur</h1>
+        <EditCreateForm isEdit={false} />
+      </div>
+    </>
+
+  )
+}
+
+export default AddCollaborator
