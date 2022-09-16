7b96632c6c0d761aff0b3fa3773cf52b402bfcb0
KAYA jordan <kayajordan21@gmail.com>
2022-09-17 01:45:18 +0200

    add stylesheet

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,16 +7,14 @@ import { Link } from 'react-router-dom'
 import { RemoveFromLocalStorage } from '../../services/userService/localStorage';
 import { useNavigate } from "react-router-dom"
 import { logOut } from '../../services/utils/utils';
+import style from "./Banner.module.css"
 
 const Banner = () => {
 
     const navigate = useNavigate()
     const user = useSelector(state => state.auth.user.user)
 
-
-    return (
-        <header>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ minHeight: "100px" }}>
+    {/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{ minHeight: "100px" }}>
                 <Container>
                     <Navbar.Brand href="/direbonjour">OneSoft</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -33,10 +31,40 @@ const Banner = () => {
                 </Container>
             </Navbar>
             <div>
-                <Link to={`/modifierprofil/${user.id}`}><img src={user.photo} alt={user.lastname} /></Link>
+                
                 <p>{user.lastname} - {user.firstname}</p>
             </div>
-            <button onClick={logOut}>Déconnexion</button>
+            <button onClick={logOut}>Déconnexion</button> */}
+    return (
+        <header>
+            <div className={`container ${style.nab_bar}`}>
+                <div>
+                    <Link to={`/direbonjour`} className={`${style.nav_brand}`}>
+                        <img src="asset/logo.png" alt="logo de OneSoft" style={{ width: 80 }} />
+                        <span>Intranet</span>
+                    </Link>
+                </div>
+                <nav className={`${style.nab_bar_nav}`}>
+                    <ul className="unstyle">
+                        <li className={`${style.nav_link}`}>
+                            <Link to={`/collaborateurs`}>
+                                Collaborateurs
+                            </Link>
+                        </li>
+                        {user.isAdmin &&
+                            <li className={`${style.nav_link}`}>
+                                <Link to={`/ajoutercollaborateur`}>
+                                    Nouveau Collaborateur
+                                </Link>
+                            </li>
+                        }
+                    </ul>
+                </nav>
+                <div className={style.user_link}>
+                    <Link to={`/modifierprofil/${user.id}`}><img src={user.photo} alt={user.lastname} style={{width:50}} /></Link>
+                    <button onClick={logOut}>Déconnexion</button>
+                </div>
+            </div>
         </header>
     )
 }
