320cd4343d9f77fbee0d913e6efcd383a86f4840
KAYA jordan <kayajordan21@gmail.com>
2022-09-16 22:19:30 +0200

    add stylesheet to login page

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useState,useEffect } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom"
 import { login } from '../../features/auth/authSlice';
 import { setAccesTokenStorage } from '../../services/userService/localStorage';
 import { loginUser } from '../../services/userService/user';
+import style from "./Login.module.css"
 
 
 const Login = ({ setToken }) => {
@@ -14,8 +15,8 @@ const Login = ({ setToken }) => {
     const [password, setPassword] = useState("");
     useEffect(() => {
         //check if user already authenticated return to /direbonjour
-        if(issetToken) {
-             navigate("/direbonjour")
+        if (issetToken) {
+            navigate("/direbonjour")
         }
     }, []);
 
@@ -30,14 +31,25 @@ const Login = ({ setToken }) => {
         })()
     }, [dispatch, email, password]);
     return (
-        <section>
-            <h1>Se connecter</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder='veuillez rentrer une adresse mail' onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" placeholder="mot de passe" onChange={(e) => setPassword(e.target.value)} />
-                <button>connexion</button>
-            </form>
-        </section>
+        <main className={style.login_main}>
+            <div className={style.container}>
+                <div >
+                    <img src="asset/logo.png" alt="logo de oneSoft" title='logo de oneSoft' />
+                </div>
+                <div>
+                    <h1 className={style.login_title}>Se connecter</h1>
+                    <form onSubmit={handleSubmit} className={style.login_form}>
+                        <div>
+                            <input type="email" placeholder='veuillez rentrer une adresse mail' onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div>
+                            <input type="password" placeholder="mot de passe" onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <button>Connexion</button>
+                    </form>
+                </div>
+            </div>
+        </main>
     )
 }
 
